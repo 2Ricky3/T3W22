101523079fb7a95579611e74fa732a7d7c8b3bf9
Ricard <231197@virtualwindow.co.za>
2023-08-02 10:23:22 +0200

    Screenshots

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+//Plants array//
+
+const arrPlants = [
+  {
+    name: "Ficus tree",
+    price: 350,
+    description: "Cool Plant",
+    image: "plant1.png",
+  },
+  {
+    name: "Parlour Palm",
+    price: 400,
+    description: "Nice Plant",
+    image: "plant2.png",
+  },
+];
+
 $(document).ready(function () {
   console.log("Hello");
 
@@ -8,13 +25,40 @@ $(document).ready(function () {
 
   //Browse Page
   $("#descriptionText").hide();
+  loadPlants();
 });
+//Load all plants
 
+function loadPlants() {
+  //Loop through the list of plants
+  for (let i = 0; i < arrPlants.length; i++) {
+    const plant = arrPlants[i];
+
+    console.log(plant);
+
+    // 1: Select the plants container and the current array to it
+    $("#plantsContainer").append($("#plantCardTemplate").html());
+
+    //2: Create a variable that contains the most recently added card
+    let currentChild = $("#plantsContainer").children().eq(i);
+    console.log(currentChild);
+
+    //3: Set the content for the current plant card from the plant list of array
+    $(currentChild).find("#nameText").text(plant.name);
+    $(currentChild).find("#priceText").text(plant.price);
+    $(currentChild).find("#descriptionText").text(plant.description);
+    $(currentChild)
+      .find(".card-img-top")
+      .attr("src", "assets/" + plant.image);
+    //4:Hide the description text
+    $(currentChild).find("#descriptionText").hide();
+  }
+}
 //When the card is clicked
-$(".card").click(function () {
+$("#plantsContainer").on("click", ".card", function () {
   ///Toggle the price and description text
-  $("#priceText").toggle();
-  $("#descriptionText").toggle();
+  $(this).find("#priceText").toggle();
+  $(this).find("#descriptionText").toggle();
 
   ///Resize the image to fit
   $(".car-img-top").toggleClass("small");
